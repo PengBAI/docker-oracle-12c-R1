b38c7e00cfd8d806ef3ecf8f4a7fbd935ab5f1e2
Peng Bai <peng.bai@worldline.com>
2016-07-29 17:04:01 +0200

    update

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -e
 touch /var/log/startup.log
-tail -f /var/log/startup.log
-echo "Starting tnslsnr" >> /var/log/startup.log 2>&1
-su oracle -c "/u01/app/oracle/product/12.1.0/xe/bin/tnslsnr &"  >> /var/log/startup.log 2>&1
-echo "Starting database" >> /var/log/startup.log 2>&1
-su oracle -c 'echo startup\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba' >> /var/log/startup.log 2>&1
-echo "Starting web console on 8080" >> /var/log/startup.log 2>&1
-su oracle -c 'echo EXEC DBMS_XDB.sethttpport\(8080\)\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba' >> /var/log/startup.log 2>&1
 
-echo "Database init..." >> /var/log/startup.log 2>&1
+echo "Starting tnslsnr" 2>&1 | tee /var/log/startup.log
+su oracle -c "/u01/app/oracle/product/12.1.0/xe/bin/tnslsnr &" 2>&1 | tee /var/log/startup.log
+echo "Starting database" 2>&1 | tee /var/log/startup.log
+su oracle -c 'echo startup\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba' 2>&1 | tee /var/log/startup.log
+echo "Starting web console on 8080" 2>&1 | tee /var/log/startup.log
+su oracle -c 'echo EXEC DBMS_XDB.sethttpport\(8080\)\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba' 2>&1 | tee /var/log/startup.log
+
+echo "Database init..." 2>&1 | tee /var/log/startup.log
 for f in /entrypoint-initdb.d/*; do
     case "$f" in
-        *.sh)  echo "$0: running $f"; . "$f"  >> /var/log/startup.log 2>&1 ;; 
-        *.sql) echo "$0: running $f"; su oracle -c "echo \@$f\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba" >> /var/log/startup.log 2>&1 ;;
-        *)     echo "No volume sql script, ignoring $f"  >> /var/log/startup.log 2>&1 ;;
+        *.sh)  echo "$0: running $f"; . "$f"  2>&1 | tee /var/log/startup.log ;; 
+        *.sql) echo "$0: running $f"; su oracle -c "echo \@$f\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba" 2>&1 | tee /var/log/startup.log ;;
+        *)     echo "No volume sql script, ignoring $f"  2>&1 | tee /var/log/startup.log ;;
     esac
     echo
 done
-echo "End init." >> /var/log/startup.log 2>&1
+echo "End init." 2>&1 | tee /var/log/startup.log1
 
-echo "Oracle started Successfully !" >> /var/log/startup.log 2>&1
+echo "Oracle started Successfully !" 2>&1 | tee /var/log/startup.log
 
 while true; do
     sleep 1m
