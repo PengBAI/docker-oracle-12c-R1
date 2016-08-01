fd09c4a5d63d4ea034e1e301540ba2984075131b
Peng Bai <peng.bai@worldline.com>
2016-08-01 10:01:25 +0200

    fix stdout

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 set -e
-touch /var/log/startup.log
 
-echo "Starting tnslsnr" 2>&1 | tee -a /var/log/startup.log
-su oracle -c "/u01/app/oracle/product/12.1.0/xe/bin/tnslsnr &" 2>&1 | tee -a /var/log/startup.log
-echo "Starting database" 2>&1 | tee -a /var/log/startup.log
-su oracle -c 'echo startup\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba' 2>&1 | tee -a /var/log/startup.log
-echo "Starting web console on 8080" 2>&1 | tee -a /var/log/startup.log
-su oracle -c 'echo EXEC DBMS_XDB.sethttpport\(8080\)\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba' 2>&1 | tee -a /var/log/startup.log
+echo "Starting tnslsnr"
+su oracle -c "/u01/app/oracle/product/12.1.0/xe/bin/tnslsnr &"
+echo "Starting database"
+su oracle -c 'echo startup\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba'
+echo "Starting web console on 8080"  
+su oracle -c 'echo EXEC DBMS_XDB.sethttpport\(8080\)\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba'
 
-echo "Database init..." 2>&1 | tee -a /var/log/startup.log
+echo "Database init..." 
 for f in /entrypoint-initdb.d/*; do
     case "$f" in
-        *.sh)  echo "$0: running $f"; . "$f"  2>&1 | tee -a /var/log/startup.log ;; 
-        *.sql) echo "$0: running $f"; su oracle -c "echo \@$f\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba" 2>&1 | tee -a /var/log/startup.log ;;
-        *)     echo "No volume sql script, ignoring $f"  2>&1 | tee -a /var/log/startup.log ;;
+        *.sh)  echo "$0: running $f"; . "$f" ;; 
+        *.sql) echo "$0: running $f"; su oracle -c "echo \@$f\; | $ORACLE_HOME/bin/sqlplus -S / as sysdba" ;;
+        *)     echo "No volume sql script, ignoring $f" ;;
     esac
     echo
 done
-echo "End init." 2>&1 | tee -a /var/log/startup.log1
+echo "End init." 
 
-echo "Oracle started Successfully !" 2>&1 | tee -a /var/log/startup.log
+echo "Oracle started Successfully !" 
 
 while true; do
     sleep 1m
